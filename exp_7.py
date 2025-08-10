e7e9100dc6877a21d9a87f5b87af096a720e4f6e
fathimarishana <fathimarishana835@gmail.com>
2025-08-10 23:57:51 +0530

    Update exp_7.py

--- exp_7.py
+++ exp_7.py
@@ -1,19 +1,6 @@
 from datetime import datetime, timedelta
-
-# Ask the user how many days to subtract
-try:
-    days_to_subtract = int(input("Enter the number of days to subtract from today's date: "))
-
-    # Get today's date
-    current_date = datetime.today()
-
-    # Subtract the days
-    new_date = current_date - timedelta(days=days_to_subtract)
-
-    # Print the result
-    print("Current date:", current_date.strftime("%Y-%m-%d"))
-    print(f"Date after subtracting {days_to_subtract} days:", new_date.strftime("%Y-%m-%d"))
-
-except ValueError:
-    print("Please enter a valid number.")
+current_date = datetime.now()
+new_date = current_date - timedelta(days=5)
+print("current date:",current_date.strftime("%y-%m-%d"))
+print("date after subtracting 5 days:",new_date.strftime("%y-%m-%d"))
 
